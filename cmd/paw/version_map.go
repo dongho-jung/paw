d9e59f3de410302010b30e05a848d7f3994be41e
dongho-jung <dongho971220@gmail.com>
2026-01-26 08:19:19 +0900

    chore: update version map for v0.9.5

--- cmd/paw/version_map.go
+++ cmd/paw/version_map.go
@@ -36,5 +36,6 @@ var versionCommitMap = map[string]string{
 	"v0.9.1": "b4910c9",
 	"v0.9.2": "dc2c919",
 	"v0.9.3": "d4a72c5",
-	"v0.9.4": "c1524d6",
+	"v0.9.4": "1c6f01a",
+	"v0.9.5": "243ce22",
 }
